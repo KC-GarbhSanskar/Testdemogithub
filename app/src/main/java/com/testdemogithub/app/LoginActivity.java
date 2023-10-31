8447d0efb2170c32791bee6bebb395a9644ccb56
kc13 <developercr2022@gmail.com>
2023-10-31 17:00:20 +0530

    Login module added

--- app/src/main/java/com/testdemogithub/app/LoginActivity.java
+++ app/src/main/java/com/testdemogithub/app/LoginActivity.java
@@ -5,11 +5,11 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class FirstActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.first_activity_layout);
+        setContentView(R.layout.login_layout);
     }
 }
